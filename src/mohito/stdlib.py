a4c701d3df870d3f6e3f6206fa01fc597c8fe6f9
samedit66 <samedit66@yandex.ru>
2025-07-25 16:57:12 +0300

    feat(stdlib): standard stack shuffling primitives

---
+++ src/mohito/stdlib.py
@@ -0,0 +1,57 @@
+from mohito import executor
+
+
+vocab = executor.Vocab(
+    parent_vocab=None,
+    builtins={},
+    user_defined={},
+)
+
+
+@vocab.builtin("dup")
+def dup(word, state, vocab, read_word, execute):
+    try:
+        state.dup()
+    except IndexError:
+        loc = word.location
+        raise RuntimeError(
+            f"{loc}: '{word.value}' expected a single element on top of the stack"
+        )
+
+
+@vocab.builtin("drop")
+def drop(word, state, vocab, read_word, execute):
+    try:
+        state.pop()
+    except IndexError:
+        loc = word.location
+        raise RuntimeError(
+            f"{loc}: '{word.value}' expected a single element on top of the stack"
+        )
+
+
+@vocab.builtin("dip")
+def dip(word, state, vocab, read_word, execute):
+    try:
+        quotation = state.pop()
+        top = state.pop()
+        execute(quotation)
+        state.push(top)
+    except IndexError:
+        loc = word.location
+        raise RuntimeError(
+            f"{loc}: '{word.value}' expected a closure on top of the stack"
+        )
+
+
+@vocab.builtin("swap")
+def swap(word, state, vocab, read_word, execute):
+    try:
+        a, b = state.pop(), state.pop()
+        state.push(a)
+        state.push(b)
+    except IndexError:
+        loc = word.location
+        raise RuntimeError(
+            f"{loc}: '{word.value}' expected 2 elements on top of the stack"
+        )
